93aec6dda5648f2830c9d2cb84b17e22c96c925f
Michael Vöhringer <zaehlerjs@voehringer.net>
2014-01-03 12:06:32 +0100

    removed require.js

--- server.js
+++ server.js
@@ -5,97 +5,74 @@
  * @author Michel Vöhringer
  */
 
-/**
- * RequireJS
- * @see http://requirejs.org/docs/node.html
- */
-var requirejs = require('requirejs');
-requirejs.config({
-  nodeRequire: require
-});
+var Http = require('http'),
+    Mongo = require('mongodb'),
+    Path = require('path'),
+    Express =  require('express'),
+    Config = require('config'),
+    Socketio = require('socket.io'),
+    Channels = require('./routes/channels'),
+    Data = require('./routes/data');
 
-/**
- * Express
- * @see http://expressjs.com/guide.html
- */
-requirejs([ 'http', 
-            'mongodb', 
-            'path', 
-            'express', 
-            'config',
-            'socket.io', 
-            './routes/channels',
-            './routes/data' 
-            ], 
 
-            function(Http,
-                    Mongo, 
-                    Path, 
-                    Express, 
-                    Config,
-                    Socketio, 
-                    Channels,
-                    Data){
+// Initiate express
+var app = Express();
 
-  // Initiate express
-  var app = Express();
+// Load MongoDB
+var db = new Mongo.Db(Config.ZaehlerJS.db.Name, 
+                      new Mongo.Server(Config.ZaehlerJS.db.Host, 
+                                       Config.ZaehlerJS.db.Port, {
+                                          native_parser: false,
+                                          auto_reconnect: true
+                                        }),{
+                          safe:false    
+                      });
 
-  // Load MongoDB
-  var db = new Mongo.Db(Config.ZaehlerJS.db.Name, 
-                        new Mongo.Server(Config.ZaehlerJS.db.Host, 
-                                         Config.ZaehlerJS.db.Port, {
-                                            native_parser: false,
-                                            auto_reconnect: true
-                                          }),{
-                            safe:false    
-                        });
+db.open(function(err, db) {
+  if (err) {
+    return console.log('\u001b[31mFailed to connect to MongoDB: ' + err + '\033[0m');
+  } else {
+    console.log('\u001b[32mConnect to MongoDB\033[0m');
 
-  db.open(function(err, db) {
-    if (err) {
-      return console.log('\u001b[31mFailed to connect to MongoDB: ' + err + '\033[0m');
-    } else {
-      console.log('\u001b[32mConnect to MongoDB\033[0m');
+    // Create index
+    db.collection('data', function(err, collection) {
+        collection.ensureIndex("medadata.date",function(data){
+            console.log('\u001b[32mindex checked\033[0m');
+        });
+        collection.ensureIndex("medadata.channel",function(data){
 
-      // Create index
-      db.collection('data', function(err, collection) {
-          collection.ensureIndex("medadata.date",function(data){
-              console.log('\u001b[32mindex checked\033[0m');
-          });
-          collection.ensureIndex("medadata.channel",function(data){
+        });
+    });
 
-          });
-      });
+    // Start http server
+    var server = Http.createServer(app).listen(Config.ZaehlerJS.server.Port, function() {
+      console.log('\u001b[32mZählerJS listening on port \u001b[33m%d\033[0m', Config.ZaehlerJS.server.Port);
+    });
 
-      // Start http server
-      var server = Http.createServer(app).listen(Config.ZaehlerJS.server.Port, function() {
-        console.log('\u001b[32mZählerJS listening on port \u001b[33m%d\033[0m', Config.ZaehlerJS.server.Port);
-      });
+    // configure express server
+    app.configure(function () {
+      app.set('config', Config.ZaehlerJS);
+      app.set('db', db);
+      app.use(Express.logger('dev'));                         /* 'default', 'short', 'tiny', 'dev' */
+      app.use(Express.urlencoded());
+      app.use(Express.json());
+      app.use(Express.compress());                            /* enable gzip compression */
+      app.use(Express.static(Path.join(__dirname, 'public'))); /* serve static files */
+      app.use(Express.favicon(__dirname + '/public/icon/favicon.ico')); /* add favicon */
+    });
+    
+    //  Configure routes
+    app.get('/channels', Channels.findAll);
+    app.get('/channels/:id', Channels.findById);
+    app.post('/channels', Channels.addChannel);
+    app.put('/channels/:id', Channels.updateChannel);
+    app.delete('/channels/:id', Channels.deleteChannel);
 
-      // configure express server
-      app.configure(function () {
-        app.set('config', Config.ZaehlerJS);
-        app.set('db', db);
-        app.use(Express.logger('dev'));                         /* 'default', 'short', 'tiny', 'dev' */
-        app.use(Express.urlencoded());
-        app.use(Express.json());
-        app.use(Express.compress());                            /* enable gzip compression */
-        app.use(Express.static(Path.join(__dirname, 'public'))); /* serve static files */
-        app.use(Express.favicon(__dirname + '/public/icon/favicon.ico')); /* add favicon */
-      });
-      
-      //  Configure routes
-      app.get('/channels', Channels.findAll);
-      app.get('/channels/:id', Channels.findById);
-      app.post('/channels', Channels.addChannel);
-      app.put('/channels/:id', Channels.updateChannel);
-      app.delete('/channels/:id', Channels.deleteChannel);
+    app.post('/data/:id.json', Data.addData);
+    app.post('/data/:id', Data.addData);
+    app.post('/data/demo/:id', Data.addDemoData);
+    app.get('/data/:id', Data.find);
 
-      app.post('/data/:id.json', Data.addData);
-      app.post('/data/:id', Data.addData);
-      app.post('/data/demo/:id', Data.addDemoData);
-      app.get('/data/:id', Data.find);
-
-    }
-  });
+  }
 });
 
